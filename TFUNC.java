fb74115f5b2d826971c62f45b590a5a11e6fe701
ninatarigan <ninatheresiatarigan2606@gmail.com>
2025-11-11 21:03:52 +0700

    NINA DAN NAYLA

--- TFUNC.java
+++ TFUNC.java
@@ -1,16 +1,15 @@
-
 import java.util.*;
 import java.lang.Math;
 
-public class TFUNC{
+public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int angka;
+        int angka, result;
 
         angka = Integer.parseInt(input.nextLine());
-        angka = hitungrekursif(angka);
-        System.out.println(angka);
+        result = hitungrekursif(angka);
+        System.out.println(result);
     }
     
     public static int hitungrekursif(int angka) {
